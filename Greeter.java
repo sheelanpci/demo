67c8e239d00feaa104f45f2a8cb69695adefa4f8
sheelanpci <33517464+sheelanpci@users.noreply.github.com>
2017-11-13 17:00:08 +0530

    Greeter.java
    java file

---
+++ Greeter.java
@@ -0,0 +1,15 @@
+package example;
+
+public class Greeter {
+
+	
+
+		public Greeter() {
+
+		  }
+
+		
+		  public String greet(String someone) {
+		    return String.format("Hello, %s!", someone);
+		  }
+	}
